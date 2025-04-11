5262576632ef3878fc3fdfea4182687610d43caf
sylvestredavid <david.sylvestre.24@gmail.com>
2025-04-11 21:58:53 +0200

    aerer la table

--- app/src/GameDisplay.tsx
+++ app/src/GameDisplay.tsx
@@ -12,7 +12,7 @@ export const GameDisplay: FC<GameDisplayProps> = () => {
   const margin = { top: 7, left: 0, right: 30, bottom: 0 }
   return (
     <>
-      <GameTable xMin={-50} xMax={50} yMin={-30} yMax={30} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
+      <GameTable xMin={-80} xMax={80} yMin={-40} yMax={40} margin={margin} css={process.env.NODE_ENV === 'development' && tableBorder}>
         <GameTableNavigation />
         <PlayerPanels />
       </GameTable>
--- app/src/locators/PlayerLayoutLocator.ts
+++ app/src/locators/PlayerLayoutLocator.ts
@@ -7,7 +7,7 @@ class PlayerLayoutLocator extends Locator {
     const base = this.getBaseCoordinates(location, context)
     const xBase = base.x ?? 0
     const yBase = base.y ?? 0
-    if (location.x === undefined) return { x: xBase + 18, y: yBase }
+    if (location.x === undefined) return { x: xBase + 21, y: yBase }
 
     const xLocation = location.x ?? 0
     const yLocation = location.y ?? 0
@@ -17,32 +17,32 @@ class PlayerLayoutLocator extends Locator {
     const index = getRelativePlayerIndex(context, location.player)
     switch (index) {
       case 0:
-        if (context.rules.players.length === 2) return { x: -45, y: 6 }
-        if (context.rules.players.length === 3) return { x: -45, y: 6 }
-        if (context.rules.players.length === 4) return { x: -45, y: 13 }
-        if (context.rules.players.length === 5) return { x: -45, y: 13 }
-        return { x: -45, y: 6 }
+        if (context.rules.players.length === 2) return { x: -70 }
+        if (context.rules.players.length === 3) return { x: -70, y: 24 }
+        if (context.rules.players.length === 4) return { x: -70, y: 24 }
+        if (context.rules.players.length === 5) return { x: -75, y: 24 }
+        return { x: -21, y: 25 }
       case 1:
-        if (context.rules.players.length === 2) return { x: 15, y: 6 }
-        if (context.rules.players.length === 3) return { y: -17 }
-        if (context.rules.players.length === 4) return { x: -45, y: -13 }
-        if (context.rules.players.length === 5) return { x: -45, y: -13 }
-        return { x: -45, y: -6 }
+        if (context.rules.players.length === 2) return { x: 28 }
+        if (context.rules.players.length === 3) return { x: -20, y: -25 }
+        if (context.rules.players.length === 4) return { x: -70, y: -25 }
+        if (context.rules.players.length === 5) return { x: -75, y: -10 }
+        return { x: -75, y: 14 }
       case 2:
-        if (context.rules.players.length === 3) return { x: 15, y: 6 }
-        if (context.rules.players.length === 4) return { x: 15, y: -13 }
-        if (context.rules.players.length === 5) return { y: -17 }
-        return { y: -17 }
+        if (context.rules.players.length === 3) return { x: 28, y: 24 }
+        if (context.rules.players.length === 4) return { x: 28, y: 24 }
+        if (context.rules.players.length === 5) return { x: -21, y: -25 }
+        return { x: -75, y: -15 }
       case 3:
-        if (context.rules.players.length === 4) return { x: 15, y: 13 }
-        if (context.rules.players.length === 5) return { x: 15, y: -13 }
-        return { x: 15, y: -6 }
+        if (context.rules.players.length === 4) return { x: 28, y: -24 }
+        if (context.rules.players.length === 5) return { x: 33, y: -10 }
+        return { x: -21, y: -25 }
       case 4:
-        if (context.rules.players.length === 5) return { x: 15, y: 13 }
-        return { x: 15, y: 6 }
+        if (context.rules.players.length === 5) return { x: 33, y: 24 }
+        return { x: 33, y: -15 }
       case 5:
       default:
-        return { y: 17 }
+        return { x: 33, y: 14 }
     }
   }
 
@@ -54,7 +54,7 @@ class PlayerLayoutLocator extends Locator {
 }
 
 export class PlayerLayoutSpotDescription extends DropAreaDescription {
-  width = 42
+  width = 49
   height = 24
 }
 
