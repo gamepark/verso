4497b81a97d06140017faae5817e414d52ddc654
sylvestredavid <david.sylvestre.24@gmail.com>
2025-04-15 19:05:36 +0200

    boutons dans historique

--- app/src/history/components/PlayCardHistory.tsx
+++ app/src/history/components/PlayCardHistory.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 
-import { MoveComponentProps, usePlayerName } from '@gamepark/react-game'
+import { MoveComponentProps, PlayMoveButton, usePlayerName } from '@gamepark/react-game'
+import { MaterialMoveBuilder } from '@gamepark/rules-api'
 import { CardItem } from '@gamepark/verso/material/Face'
 import { MaterialType } from '@gamepark/verso/material/MaterialType'
 import { FaceCardHelper } from '@gamepark/verso/rules/helpers/FaceCardHelper'
@@ -13,10 +14,19 @@ export const PlayCardHistory = (props: MoveComponentProps) => {
   const card: CardItem = context.game.items[MaterialType.Card][move.itemIndex]
   const value = FaceCardHelper.getCardValue(card)
   const color = FaceCardHelper.getCardColor(card)
+  const displayMaterialHelp = MaterialMoveBuilder.displayMaterialHelp
 
   if (value === 0) {
-    return <Trans defaults="history.place.joker.player" values={{ player: name, color }} />
+    return (
+      <Trans defaults="history.place.joker.player" values={{ player: name, color }}>
+        <PlayMoveButton move={displayMaterialHelp(MaterialType.Card, card)} local />
+      </Trans>
+    )
   }
 
-  return <Trans defaults="history.place.card.player" values={{ player: name, value, color }} />
+  return (
+    <Trans defaults="history.place.card.player" values={{ player: name, value, color }}>
+      <PlayMoveButton move={displayMaterialHelp(MaterialType.Card, card)} local />
+    </Trans>
+  )
 }
