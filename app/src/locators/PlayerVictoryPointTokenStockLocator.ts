9ccd6750eca5c3b14fb3835e4b7cbee3b25396f0
sylvestredavid <david.sylvestre.24@gmail.com>
2025-04-18 18:49:48 +0200

    display 5 player

--- app/src/locators/PlayerVictoryPointTokenStockLocator.ts
+++ app/src/locators/PlayerVictoryPointTokenStockLocator.ts
@@ -11,25 +11,25 @@ class PlayerVictoryPointTokenStockLocator extends PileLocator {
         if (context.rules.players.length === 2) return { x: -40, y: 3 }
         if (context.rules.players.length === 3) return { x: -50, y: 3 }
         if (context.rules.players.length === 4) return { x: -50, y: 3 }
-        if (context.rules.players.length === 5) return { x: -55, y: 20 }
+        if (context.rules.players.length === 5) return { x: -18, y: 28 }
         return { x: -14, y: 25 }
       case 1:
         if (context.rules.players.length === 2) return { x: 40, y: 3 }
         if (context.rules.players.length === 3) return { x: -22, y: -30 }
         if (context.rules.players.length === 4) return { x: -14, y: -30 }
-        if (context.rules.players.length === 5) return { x: -55, y: -10 }
+        if (context.rules.players.length === 5) return { x: -55, y: -26 }
         return { x: -55, y: 15 }
       case 2:
         if (context.rules.players.length === 3) return { x: 50, y: 3 }
         if (context.rules.players.length === 4) return { x: 14, y: -30 }
-        if (context.rules.players.length === 5) return { x: -14, y: -20 }
+        if (context.rules.players.length === 5) return { x: -22, y: -30 }
         return { x: -55, y: -15 }
       case 3:
         if (context.rules.players.length === 4) return { x: 50, y: 3 }
-        if (context.rules.players.length === 5) return { x: 25, y: -10 }
+        if (context.rules.players.length === 5) return { x: 50, y: -26 }
         return { x: -14, y: -25 }
       case 4:
-        if (context.rules.players.length === 5) return { x: 25, y: 20 }
+        if (context.rules.players.length === 5) return { x: 17, y: 28 }
         return { x: 25, y: -15 }
       case 5:
       default:
