e5c4a3d0841478809faa23c0cc3e589c2115eae5
sylvestredavid <david.sylvestre.24@gmail.com>
2025-04-25 18:33:18 +0200

    header simultaneous rule

--- app/src/headers/BankLastSequenceHeader.tsx
+++ app/src/headers/BankLastSequenceHeader.tsx
@@ -1,17 +1,28 @@
 /** @jsxImportSource @emotion/react */
-import { PlayMoveButton, useLegalMove } from '@gamepark/react-game'
+import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { isCustomMoveType } from '@gamepark/rules-api'
 import { CustomMoveType } from '@gamepark/verso/rules/CustomMoveType'
-import { Trans } from 'react-i18next'
+import { VersoRules } from '@gamepark/verso/VersoRules'
+import { Trans, useTranslation } from 'react-i18next'
 
 export const BankLastSequenceHeader = () => {
+  const { t } = useTranslation()
+  const playerId: number | undefined = usePlayerId()
+  const activePlayers = useRules<VersoRules>()?.game.rule?.players ?? []
+  const player = usePlayerName(activePlayers[0])
   const validate = useLegalMove(isCustomMoveType(CustomMoveType.Score))
-  return (
-    <Trans
-      defaults="header.bank.last.you"
-      components={{
-        validate: <PlayMoveButton move={validate} />
-      }}
-    />
-  )
+  if (playerId !== undefined && activePlayers.includes(playerId)) {
+    return (
+      <Trans
+        defaults="header.bank.last.you"
+        components={{
+          validate: <PlayMoveButton move={validate} />
+        }}
+      />
+    )
+  } else if (activePlayers.length === 1) {
+    return <>{t('header.bank.last.player', { player })}</>
+  } else {
+    return <>{t('header.bank.last.others')}</>
+  }
 }
