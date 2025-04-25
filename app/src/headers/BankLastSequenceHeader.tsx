f41a32ab48c07a83dc46aa8d43f93f25fa1c5934
Romain Fromi <romain.fromi@gmail.com>
2025-04-25 15:09:10 +0200

    Disable passing when declaring the last sequence

--- app/src/headers/BankLastSequenceHeader.tsx
+++ app/src/headers/BankLastSequenceHeader.tsx
@@ -1,19 +1,17 @@
 /** @jsxImportSource @emotion/react */
-
 import { PlayMoveButton, useLegalMove } from '@gamepark/react-game'
-import { isCustomMoveType, isEndPlayerTurn } from '@gamepark/rules-api'
+import { isCustomMoveType } from '@gamepark/rules-api'
 import { CustomMoveType } from '@gamepark/verso/rules/CustomMoveType'
-import { ScoreType } from '@gamepark/verso/rules/ScoreType'
 import { Trans } from 'react-i18next'
 
 export const BankLastSequenceHeader = () => {
-  const pass = useLegalMove(isEndPlayerTurn)
-  const validate = useLegalMove((move) => isCustomMoveType(CustomMoveType.Score)(move) && move.data.type === ScoreType.Sequence)
-
+  const validate = useLegalMove(isCustomMoveType(CustomMoveType.Score))
   return (
-    <Trans defaults="header.bank.last.you">
-      <PlayMoveButton move={pass} />
-      <PlayMoveButton move={validate} />
-    </Trans>
+    <Trans
+      defaults="header.bank.last.you"
+      components={{
+        validate: <PlayMoveButton move={validate} />
+      }}
+    />
   )
 }
--- rules/src/rules/BankLastSequenceRule.ts
+++ rules/src/rules/BankLastSequenceRule.ts
@@ -4,9 +4,7 @@ import { Scoring } from './ScoreType'
 
 export class BankLastSequenceRule extends SimultaneousRule {
   getActivePlayerLegalMoves(player: number) {
-    const pass = this.endPlayerTurn(player)
-    const helper = new BankSequenceHelper(this.game, player)
-    return [pass, ...helper.getSequenceMoves()]
+    return new BankSequenceHelper(this.game, player).getSequenceMoves()
   }
 
   onCustomMove(move: CustomMove): MaterialMove[] {
